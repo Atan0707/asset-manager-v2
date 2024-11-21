12aee4fcb42a3c1e5f4e445d697dce829d62e739
Hariz Hakim <harizhakim85@gmail.com>
2024-11-21 21:48:30 +0800

    added encryption method

--- app/test-1/page.tsx
+++ app/test-1/page.tsx
@@ -1,9 +1,19 @@
+'use client';
 import React from 'react'
+import keccak256 from 'keccak256';
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    const hash = keccak256((Buffer.from("atan")));
+    const atan = keccak256("atan").toString("base64")
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>{keccak256("atan")}</p>
+            <p>{hash}</p>
+            <p>{atan}</p>
+        </div>
+    )
 }
 
 export default page
